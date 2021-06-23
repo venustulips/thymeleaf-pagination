b3f4f214a192fbd9b2ff374006766aebfb35fdea
venustulips <venustulips@gmail.com>
2021-06-23 15:48:46 +0630

    initial project generation

---
+++ src/main/java/com/example/thymeleafpagination/ThymeleafPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafpagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPaginationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafPaginationApplication.class, args);
+    }
+
+}
